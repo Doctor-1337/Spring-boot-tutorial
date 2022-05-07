19e32d84a42c7018085cebfe743f96853edc0085
Doctor1337 <iamnabeel99@gmail.com>
2022-05-07 19:43:46 +0530

    replaced properties to yaml properties file

--- src/main/java/com/doc/Springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/doc/Springboot/tutorial/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.doc.Springboot.tutorial.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+
     @GetMapping("/")
     public String sayHello(){
-       return "Hi my 1st API using Spring";
+       return welcomeMessage;
     }
 
 
